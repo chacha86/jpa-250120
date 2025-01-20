c1b8f0cd54d1a3c878891cfbcccb4212c5ff8be8
chacha86 <taejincha0619@gmail.com>
2025-01-20 11:51:40 +0900

    post modify

--- src/main/java/com/example/jpa/global/BaseInitData.java
+++ src/main/java/com/example/jpa/global/BaseInitData.java
@@ -1,10 +1,12 @@
 package com.example.jpa.global;
 
+import com.example.jpa.domain.post.post.entity.Post;
 import com.example.jpa.domain.post.post.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 
 @Configuration
 @RequiredArgsConstructor
@@ -13,11 +15,10 @@ public class BaseInitData {
     private final PostService postService;
 
     @Bean
+    @Order(1)
     public ApplicationRunner applicationRunner() {
         return args -> {
             // 샘플 데이터 3개 생성.
-
-
             // 데이터가 3개가 이미 있으면 패스
             if( postService.count() > 0 ) {
                 return ;
@@ -27,6 +28,15 @@ public class BaseInitData {
             postService.write("title2", "body2");
             postService.write("title3", "body3");
 
+        };
+    }
+
+    @Bean
+    @Order(2)
+    public ApplicationRunner applicationRunner2() {
+        return args -> {
+            Post post = postService.findById(1L).get();
+            postService.modify(post, "new title", "new body");
         };
     }
 
