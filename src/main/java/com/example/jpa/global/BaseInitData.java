86f86934db083376a75c993e04dc7dcf1dd9368d
chacha86 <taejincha0619@gmail.com>
2025-01-22 11:49:00 +0900

    test profile add

--- src/main/java/com/example/jpa/global/BaseInitData.java
+++ src/main/java/com/example/jpa/global/BaseInitData.java
@@ -38,15 +38,7 @@ public class BaseInitData {
 
     @Transactional
     public void work2() {
-        Post post = postService.findById(1L).get();
 
-        // 외래키 제약 때문에 자식이 있는 post는 바로 지울 수 없다. post가 가지고 있는 댓글 3개를 먼저 지워야 한다.
-//        for(Comment comment : post.getComments()) {
-//            commentService.delete(comment);
-//        }
-
-        // cascade.REMOVE를 걸면 JPA가 알아서 지워준다.
-        postService.delete(post);
     }
 
     @Transactional
